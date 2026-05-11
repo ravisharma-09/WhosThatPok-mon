7b5e3ba882f565f628cb7b9139ddcb619e4b0ffe
ravisharma-09 <sharmaravi75080@gmail.com>
2026-05-12 00:13:57 +0530

    grid and readmee

---
+++ Readme.md
@@ -0,0 +1,34 @@
+# WhosThatPokémon 
+
+A pokemon stats website built with Html, Css , javaScript using the PokeApi 
+
+## live demo 
+
+
+## preivew 
+
+
+## features
+- search any Pkemon by name in search bar it will its stats
+- view stats , type , abilities,height,sprite,weight 
+- stat bars with visual display 
+- Browse all pokemon in a grid 
+- Load more 
+- Responsive (mobile freindly)
+
+## Built with 
+- HTML
+- CSS
+- JavaScript
+- [pokeAPI](https://pokeapi.co/)
+
+## What I Learned  "it was more learning then a project"
+
+- async / await
+- fetch API 
+- Promise.all for parallel request
+## how to run 
+- clone the repo
+- open index.html in your browser
+
+
--- main.js
+++ main.js
@@ -12,11 +12,14 @@ async function fetchpoki(name) {
     }
     catch(error){
         console.error(error)
+        display.innerHTML = `<h2> cant find this pokemon</h2>`
     }    
 }
 
 
 
+
+let offset = 0 ;
 const btn = document.getElementById("btn") ;
 const inpo = document.getElementById("inpo") ;
 
@@ -75,3 +78,67 @@ function closecard(){
     display.style.display = "none" ;
     inpo.value = "" ;
 }
+
+async function fetchbatch(){
+    console.log(offset)
+    try{
+        const promises = []
+        for(let i = offset+1 ; i<= offset +15 ; i++){
+            promises.push(
+                fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+            )
+        }
+        const result = await Promise.all(promises);
+        let ans = await Promise.all(result.map(v => v.json()))
+
+       
+
+        ans.forEach(element => { rendercardgrid(element)
+            offset +=1
+            
+        });
+
+    }
+    catch(error){
+        console.error(error)
+    }
+
+}
+
+
+function rendercardgrid(data){
+
+    const name = data.name ;
+    const id = data.id ;
+    const sprite = data.sprites.other["official-artwork"].front_default ;
+    const types = data.types.map(t => t.type.name) ;
+    const weight = data.weight /10
+    const height = data.height / 10
+    const stats = data.stats.map(s =>({
+        name : s.stat.name,
+        value : s.base_stat 
+    }))
+    const ability = data.abilities.map(t => t.ability.name);
+    const exp = data.base_experience ;
+    
+
+const card = document.createElement("div") ;
+card.classList.add("pokecard")
+card.innerHTML = `
+    <h3>#${id} ${name}</h3>
+    <img src="${sprite}" alt="${name}" width="200" height="300">
+    <p>Heigth: ${height} m | Weight: ${weight} kg</p>
+    <p>type: ${types.join(', ')}</p>
+    <p>Abilities: ${ability.join(', ')}</P>
+    <p class="exp">Base EXP: ${exp}</P>
+
+`
+document.getElementById('gridcards').appendChild(card)
+}
+function load(){
+fetchbatch()
+}
+fetchbatch()
+
+
+
